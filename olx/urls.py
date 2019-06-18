1085ea4a8780f5a336d32d989a06b8c8cf262515
Slava <ant.form@icloud.com>
2019-06-18 19:25:17 +0700

    time chart

--- olx/urls.py
+++ olx/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
         path('', TemplateView.as_view(template_name="olx/home.html"), name='home'),
         path('olx/', TemplateView.as_view(template_name="olx/olx.html"), name='olx'),
         path('olx/request', views.RequestView.as_view(), name='olx_request'),
+        path('olx/p_time/<int:pk>', views.PopularTime.as_view(), name='olx_p_time'),
+        path('olx/p_day/<int:pk>', views.PopularDay.as_view(), name='olx_p_day'),
         ]
--- olx/views.py
+++ olx/views.py
@@ -4,6 +4,12 @@ from django.views.generic.edit import FormView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from olx.tasks import get_request
+from django.views import View
+from django.shortcuts import render
+from olx.models import OlxRequest
+import gviz_api
+
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -19,3 +25,25 @@ class RequestView(SuccessMessageMixin, FormView):
         get_request.delay(model.pk)
 
         return super().form_valid(form)
+
+class PopularTime(View):
+    def get(self, request, pk):
+        description={"hour" : ("time", "Time"), "quantity" : ("number", "Quantity")}
+        data = []
+        context = {}
+        obj = OlxRequest.objects.get(pk = pk)
+
+        for x in range(24):
+            dic={}
+            dic['hour'] = x
+            dic['quantity'] = obj.olxlinks_set.filter(datetime__hour=x).count()
+            data.append(dic)
+
+        data_table = gviz_api.DataTable(description)
+        data_table.LoadData(data)
+        context['gviz_data'] = data_table.ToJSon()
+
+        return render(request, 'olx/p_time.html', context)
+
+class PopularDay(View):
+    pass
